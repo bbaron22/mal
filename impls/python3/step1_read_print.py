e1e0cd55239ee93a306a57757c11e18529479774
Bruce Baron <77284591+bbaron22@users.noreply.github.com>
2022-07-24 12:30:16 -0400

    imported json_parser

---
+++ impls/python3/step1_read_print.py
@@ -0,0 +1,41 @@
+import sys
+import traceback
+import json
+json.load()
+
+import mal_readline
+
+
+def READ(s):
+    return s
+
+
+def EVAL(ast, env):
+    return ast
+
+
+def PRINT(exp):
+    return exp
+
+
+def rep(s):
+    return PRINT(READ(EVAL(s, "")))
+
+
+def main():
+    while True:
+        # noinspection PyBroadException
+        try:
+            line = mal_readline.readline("user> ")
+            if line is None:
+                break
+            if line == "":
+                continue
+            print(rep(line))
+        except Exception:
+            print("".join(traceback.format_exception(*sys.exc_info())))
+
+
+
+if __name__ == '__main__':
+    main()
